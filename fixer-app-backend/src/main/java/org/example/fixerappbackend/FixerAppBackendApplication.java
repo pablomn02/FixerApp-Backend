ecd5c7c2b4dc914aa557030d5f29b99856717dfe
pablomn02 <pablomnavarro9@gmail.com>
2025-03-17 19:35:11 +0100

    Commit inicial

---
+++ fixer-app-backend/src/main/java/org/example/fixerappbackend/FixerAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.fixerappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixerAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixerAppBackendApplication.class, args);
+	}
+
+}
---
+++ fixer-app-backend/src/test/java/org/example/fixerappbackend/FixerAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fixerappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixerAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
